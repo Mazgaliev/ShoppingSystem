e25a8252c8a5c9d04b6ebdeff8b2db5591d8be5e
Mite <mite_mazgaliev@live.com>
2021-12-23 22:21:24 +0100

    Initial commit

---
+++ src/main/java/com/example/shoppingsystem/model/Mesto.java
@@ -0,0 +1,35 @@
+package com.example.shoppingsystem.model;
+
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class Mesto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    @ManyToOne
+    private User korisnik;
+
+//    private List<Naracka> narackaList;
+//
+//    private List<Proizvod> proizvodList;
+
+    public Mesto(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public Mesto() {
+
+    }
+}
